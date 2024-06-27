402fcb490243d7aa88e61d449bf2419bb9c6d85c
danhumassmed <daniel.higgins@umassmed.edu>
2024-06-27 16:19:23 -0400

    updated vector store

--- llm_analyst/documents/vector_store.py
+++ llm_analyst/documents/vector_store.py
@@ -6,11 +6,9 @@ import hashlib
 import os
 import re
 
-from langchain_huggingface import HuggingFaceEmbeddings
 
 
 from langchain_chroma import Chroma
-from langchain_community.document_loaders import TextLoader
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 
@@ -26,16 +24,18 @@ class VectorStore:
         self.cache_directory = cache_directory
         self.persist_directory = f"{cache_directory}/chroma_db"
         self.local_data_directory = local_data_directory
+        self.collection_name = os.path.basename(self.local_data_directory)
         self.local_db_hash = self._stored_db_hash()
 
         self.embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
         
         if not os.path.exists(self.persist_directory):
             os.makedirs(self.persist_directory)
-            
+        
         self.vector_db = Chroma(
+                collection_name=self.collection_name,
                 persist_directory=self.persist_directory,
-                embedding_function=self.embedding_function,
+                embedding_function=self.embedding_function
             )
             
 
@@ -54,9 +54,12 @@ class VectorStore:
                     )
                 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
                 chunked_documents = text_splitter.split_documents(documents)
-                self.vector_db = await Chroma.afrom_documents(
+                
+                
+                self.vector_db = Chroma.from_documents(
                                                     chunked_documents,
                                                     self.embedding_function,
+                                                    collection_name=self.collection_name,
                                                     persist_directory=self.persist_directory,
                                                 )
                 self.__store_db_hash()
@@ -90,7 +93,7 @@ class VectorStore:
 
     def __store_db_hash(self):
         """Save the hash to a file."""
-        hash_file = os.path.join(self.cache_directory, "local_data_directory.sha")
+        hash_file = os.path.join(self.cache_directory, f"{self.collection_name}.sha")
 
         # Ensure the directory exists
         if not os.path.exists(self.cache_directory):
@@ -104,7 +107,7 @@ class VectorStore:
 
     def _stored_db_hash(self):
         ret_val = None
-        hash_file = f"{self.cache_directory}/local_data_directory.sha"
+        hash_file = f"{self.cache_directory}/{self.collection_name}.sha"
         if os.path.exists(hash_file):
             with open(hash_file, "r", encoding="utf-8") as file:
                 ret_val = file.read().strip()
--- llm_analyst/utils/utilities.py
+++ llm_analyst/utils/utilities.py
@@ -3,6 +3,7 @@ Simple helper functions
 """
 
 import os
+import time
 import importlib
 
 
@@ -11,3 +12,39 @@ def get_resource_path():
     module_spec = importlib.util.find_spec(package_nm)
     package_path = os.path.dirname(module_spec.origin)
     return package_path
+
+
+def pretty_unique_hash():
+    mapped_chars = []
+    
+    digit_map = {
+        '00': '0', '01': '1', '02': '2', '03': '3', '04': '4',
+        '05': '5', '06': '6', '07': '7', '08': '8', '09': '9',
+        '10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E',
+        '15': 'F', '16': 'G', '17': 'H', '18': 'I', '19': 'J',
+        '20': 'K', '21': 'L', '22': 'M', '23': 'N', '24': 'O',
+        '25': 'P', '26': 'Q', '27': 'R', '28': 'S', '29': 'T',
+        '30': 'U', '31': 'V', '32': 'W', '33': 'X', '34': 'Y',
+        '35': 'Z', '36': 'a', '37': 'b', '38': 'c', '39': 'd',
+        '40': 'e', '41': 'f', '42': 'g', '43': 'h', '44': 'i',
+        '45': 'j', '46': 'k', '47': 'l', '48': 'm', '49': 'n',
+        '50': 'o', '51': 'p', '52': 'q', '53': 'r', '54': 's',
+        '55': 't', '56': 'u', '57': 'v', '58': 'w', '59': 'x',
+        '60': 'y', '61': 'z', '62': '0', '63': '1', '64': '2',
+        '65': '3', '66': '4', '67': '5', '68': '6', '69': '7',
+        '70': '8', '71': '9', '72': 'A', '73': 'B', '74': 'C',
+        '75': 'D', '76': 'E', '77': 'F', '78': 'G', '79': 'H',
+        '80': 'I', '81': 'J', '82': 'K', '83': 'L', '84': 'M',
+        '85': 'N', '86': 'O', '87': 'P', '88': 'Q', '89': 'R',
+        '90': 'S', '91': 'T', '92': 'U', '93': 'V', '94': 'W',
+        '95': 'X', '96': 'Y', '97': 'Z', '98': 'a', '99': 'b'
+    }
+    timestamp = str(int(time.time()*1000000)) 
+
+    for i in range(0, len(timestamp), 2):
+        pair = timestamp[i:i+2]
+        mapped_chars.append(digit_map[pair])
+
+    mapped_string = ''.join(mapped_chars)
+
+    return mapped_string
